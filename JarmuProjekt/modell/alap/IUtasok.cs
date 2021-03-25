fcef3add66b55004bae6794ca5633cac0dc850c8
gycsaba <gyuris.csaba@vasvari.org>
2021-03-25 15:59:07 +0100

    Modell mappa interfacekkel

---
+++ JarmuProjekt/modell/alap/IUtasok.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12GroupWork.model
+{
+    interface IUtasok
+    {
+        public void Beszall(int passangerNumber);
+        public void Kiszall(int newPassangerNumber);
+    }
+}
---
+++ JarmuProjekt/modell/alap/IUzemanyagTartaly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12GroupWork.model
+{
+    interface IUzemanyagTartaly
+    {
+        void Tolt(double uzemanyagMennyiseg);
+        void UzemanyagFogyas(double megtettUtHossza,double fogyasztas);
+    }
+}
