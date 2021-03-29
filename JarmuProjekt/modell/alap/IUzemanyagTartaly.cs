f11666209134bafc78f9322e35bd8e73abd94803
Gyuris Csaba <gyuris.csaba@vasvari.hu>
2021-03-29 10:59:06 +0200

    Update IUzemanyagTartaly.cs

--- JarmuProjekt/modell/alap/IUzemanyagTartaly.cs
+++ JarmuProjekt/modell/alap/IUzemanyagTartaly.cs
@@ -6,7 +6,18 @@ namespace _12GroupWork.model
 {
     interface IUzemanyagTartaly
     {
+        /// <summary>
+        /// Jármű tankol adott üzemanyag mennyiséget
+        /// </summary>
+        /// <param name="ujUzemanyagMennyiseg">Ennyit tankol a jármű</param>
+        /// <exception cref="KivetelTolt">Ennyi üzemanyag nem fér a tartályba.</exception>
         void Tolt(double uzemanyagMennyiseg);
+        /// <summary>
+        /// A jármű adott fogyasztással utazik adott hosszúságú utat. 
+        /// Ekkor a fogyasztásnak megfelelően csökken a tartályban lévő üzemanyaga
+        /// </summary>
+        /// <param name="megtettUtHossza">Ennyi km-t utazik a jarmű </param>
+        /// <param name="fogyasztas">100 km-en ennyit fogyaszt a jármű </param>
         void UzemanyagFogyas(double megtettUtHossza,double fogyasztas);
     }
 }
