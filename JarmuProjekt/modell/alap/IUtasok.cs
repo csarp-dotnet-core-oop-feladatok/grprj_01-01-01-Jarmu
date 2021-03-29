43af639366ab68dc2ab48494d37143118517e6fe
Gyuris Csaba <gyuris.csaba@vasvari.hu>
2021-03-29 10:57:33 +0200

    Update IUtasok.cs

--- JarmuProjekt/modell/alap/IUtasok.cs
+++ JarmuProjekt/modell/alap/IUtasok.cs
@@ -6,7 +6,15 @@ namespace _12GroupWork.model
 {
     interface IUtasok
     {
+        /// <summary>
+        /// Utasok beszállnak a járműbe
+        /// </summary>
+        /// <param name="jarmubeBeszalloUtasokSzama"></param>
         public void Beszall(int passangerNumber);
+        /// <summary>
+        /// Utasok kiszállnak a járműből
+        /// </summary>
+        /// <param name="kisszaloUtasokSzama"></param>
         public void Kiszall(int newPassangerNumber);
     }
 }
